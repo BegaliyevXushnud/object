89a7f9def615f806dd81ba294a7d819f922d190e
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-07-05 10:55:22 +0500

    first commit

---
+++ appe.js
@@ -0,0 +1,91 @@
+
+
+// 1-masala
+
+// const number = [5,7,2,8,1,9,6,3];
+// let KNumber = number[0];
+//
+// for(let i = 0; i < number.length; i++){
+//     if(number[i] > KNumber){
+//         KNumber = number[i];
+//     }
+// }
+// console.log(`katta son ${KNumber}`);
+
+
+
+
+
+
+
+
+
+
+
+// 2-masala
+// const number = [5,7,2,8,1,9,6,3];
+// let KNumber = number[0];
+//
+// for(let i = 0; i < number.length; i++){
+//     if(number[i] < KNumber){
+//         KNumber = number[i];
+//     }
+// }
+// console.log(`kichik son ${KNumber}`);
+
+
+
+
+
+
+
+
+
+// // 3-masala
+//
+// const  numbers = [5,7,2,8,1,9,6,3];
+// let UserNumber = parseInt(prompt("Son kiriting "))
+// const smallNumber = [];
+// for( let i = 0; i < numbers.length; i++ ) {
+//     if(numbers[i] < UserNumber){
+//         smallNumber.push(numbers[i])
+//     }
+// }
+// console.log(smallNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+// 4-masala
+// function Mson(num) {
+//     if(num <= 1){
+//         return false;
+//     }
+// let sum = 0;
+//     for(let i = 1; i <= num; i++) {
+//         if(num % i === 0){
+//             sum += i;
+//         }
+//     }
+//     return  sum === num;
+//
+// }
+// const userNumber  = parseInt(prompt("Son kiriting"));
+//
+// if(!isNaN(userNumber)){
+// if(Mson(userNumber)){
+//     console.log(`${userNumber}: mukammal son`);
+// }else{
+//     console.log(`${userNumber}: mukammal son emas`);
+// }
+// }else {
+//     console.log(" son kiriting");
+// }
